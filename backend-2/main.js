1a54e2ff723c8e70855d97162db91543ef7bc9a0
Joao Victor Negreiros <joaovictornsv@gmail.com>
2023-10-23 22:36:12 -0300

    adds fakes

--- backend-2/main.js
+++ backend-2/main.js
@@ -3,20 +3,64 @@ import {DatabaseName, getDatabase} from "./db.js";
 
 const app = express()
 
+const Collections = {
+  DRIVERS: 'driverDb',
+  WEIGHINGS: 'weighingDb',
+  INVOICES: 'invoiceDb',
+  LICENSE_PLATES: 'licensePlateDb'
+}
+
+const getCollection = async (collectionName) => {
+  const db = await getDatabase(DatabaseName)
+  return db.collection(collectionName)
+}
+
 app.get('/', async (req, res) => {
   res.json({ ok: true })
 })
 
-app.get('/drivers', async (req, res) => {
-  const db = await getDatabase(DatabaseName)
-  const users = await db.collection('driverDb').find().toArray()
+app.get('/drivers', async (req, res) => {c
+  const collection = await getCollection(Collections.DRIVERS)
+  const drivers = await collection.find().toArray()
 
-  res.json(users)
+  res.json(drivers)
 })
 
+app.get('/verify-license-plate/:number', async (req, res) => {
+  const {number} = req.params
+
+  res.json({
+    allowed: number === 'ABC1D34'
+  })
+})
+
+app.get('/verify-weighing/:number', async (req, res) => {
+  const {number} = req.params
+
+  res.json({
+    allowed: number === 'ABC1D34'
+  })
+})
+
+app.get('/license-plate-info', async (req, res) => {
+  res.json({
+    model: 'Fiat Uno',
+    year: 2023
+  })
+})
+
+app.get('/invoice-info', async (req, res) => {
+  res.json({
+    company: 'Company X',
+    loadItems: ["Casaco","Tecido"],
+    loadWeight: 100
+  })
+})
+
+
 app.get('/weighings', async (req, res) => {
-  const db = await getDatabase(DatabaseName)
-  const weighings =  await db.collection('weighingDb').find().toArray()
+  const collection = await getCollection(Collections.DRIVERS)
+  const weighings =  await collection.find().toArray()
 
   res.json(weighings)
 })
