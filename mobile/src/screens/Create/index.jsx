60e0852e32d20a74cf0eb41d30260b73b12fb8e4
Joao Victor Negreiros <joaovictornsv@gmail.com>
2023-10-24 22:31:04 -0300

    - mobile: adds verify buttons on create screen

--- mobile/src/screens/Create/index.jsx
+++ mobile/src/screens/Create/index.jsx
@@ -67,29 +67,34 @@ export default function CreateScreen({ navigation }) {
                         Veículo
                       </Text>
 
-                      <Controller
-                        control={control}
-                        render={({ field }) => (
-                          <Input
-                            placeholder="Número da placa"
-                            onBlur={field.onBlur}
-                            onChangeText={(data) => field.onChange(data)}
-                            value={field.value}
-                            variant="unstyled"
-                            size="lg"
-                            w="100%"
-                            style={ {
-                              backgroundColor: '#E5E0FF',
-                              color: '#0C0F14',
-                              height: 55,
-                              borderRadius: 4,
-                              marginTop: 5,
-                            }}
-                          />
-                        )}
-                        name="license_plate_number"
-                        defaultValue=""
-                      />
+                      <View style={{ flex: 1, alignItems:"center", flexDirection:'row', gap: 4}}>
+                        <Controller
+                          control={control}
+                          render={({ field }) => (
+                            <Input
+                              placeholder="Número da placa"
+                              onBlur={field.onBlur}
+                              onChangeText={(data) => field.onChange(data)}
+                              value={field.value}
+                              variant="unstyled"
+                              size="lg"
+                              w="70%"
+                              style={ {
+                                backgroundColor: '#E5E0FF',
+                                color: '#0C0F14',
+                                height: 55,
+                                borderRadius: 4,
+                                marginTop: 5,
+                              }}
+                            />
+                          )}
+                          name="license_plate_number"
+                          defaultValue=""
+                        />
+                        <Button style={{ flexGrow: 1, backgroundColor: '#7286D3'}} >
+                          <Text style={{color: '#FBFEFF', fontWeight: 'bold'}}>Verificar</Text>
+                        </Button>
+                      </View>
                       <View style={{ flex: 1, flexDirection:'row', gap: 4}}>
                         <Controller
                           control={control}
@@ -143,29 +148,35 @@ export default function CreateScreen({ navigation }) {
                         Nota Fiscal
                       </Text>
 
-                      <Controller
-                        control={control}
-                        render={({ field }) => (
-                          <Input
-                            placeholder="Código"
-                            onBlur={field.onBlur}
-                            onChangeText={(data) => field.onChange(data)}
-                            value={field.value}
-                            variant="unstyled"
-                            size="lg"
-                            w="100%"
-                            style={ {
-                              backgroundColor: '#E5E0FF',
-                              color: '#0C0F14',
-                              height: 55,
-                              borderRadius: 4,
-                              marginTop: 5,
-                            }}
-                          />
-                        )}
-                        name="invoice_number"
-                        defaultValue=""
-                      />
+                      <View style={{ flex: 1, alignItems:"center", flexDirection:'row', gap: 4}}>
+                        <Controller
+                          control={control}
+                          render={({ field }) => (
+                            <Input
+                              placeholder="Código"
+                              onBlur={field.onBlur}
+                              onChangeText={(data) => field.onChange(data)}
+                              value={field.value}
+                              variant="unstyled"
+                              size="lg"
+                              w="70%"
+                              style={ {
+                                backgroundColor: '#E5E0FF',
+                                color: '#0C0F14',
+                                height: 55,
+                                borderRadius: 4,
+                                marginTop: 5,
+                              }}
+                            />
+                          )}
+                          name="invoice_number"
+                          defaultValue=""
+                        />
+                        <Button style={{ flexGrow: 1, backgroundColor: '#7286D3'}} >
+                          <Text style={{color: '#FBFEFF', fontWeight: 'bold'}}>Verificar</Text>
+                        </Button>
+                      </View>
+
                       <View style={{ flex: 1, flexDirection:'row', gap: 4}}>
                         <Controller
                           control={control}
