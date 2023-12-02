4ae8c433e20dde54dc22c722307fc99b847f6477
luis.henrique <luis.henrique@enacom.com.br>
2023-12-02 14:58:40 -0300

    add doc

--- backend/db.js
+++ backend/db.js
@@ -10,11 +10,21 @@ const client = new MongoClient(url);
 // Database Name
 export const DatabaseName = process.env.DB_NAME;
 
+/**
+ * Conecta ao banco de dados e retorna o objeto de banco de dados.
+ * @param {string} dbName - Nome do banco de dados a ser conectado.
+ * @returns {Promise<Db>} - Retorna o objeto de banco de dados conectado.
+ */
 export const getDatabase = async (dbName) => {
   await client.connect();
   return client.db(dbName);
 };
 
+/**
+ * Conecta ao banco de dados e retorna o objeto de banco de dados.
+ * @param {string} dbName - Nome do banco de dados a ser conectado.
+ * @returns {Promise<Db>} - Retorna o objeto de banco de dados conectado.
+ */
 export const getCollection = async (collectionName) => {
   const db = await getDatabase(DatabaseName);
   return db.collection(collectionName);
